e3b17c87c101fe6d42e91b6733c5c54a5824240f
joaofelipex <felipemiranda981@gmail.com>
2025-02-05 18:41:55 -0300

    mais um save...

--- scriptestudos.sql
+++ scriptestudos.sql
@@ -279,6 +279,26 @@ insert into uf (iduf, nome, sigla) values (5, 'Rio Grande do Sul', 'RS');
 insert into uf (iduf, nome, sigla) values (6, 'Rio de Janeiro', 'RJ');
 select * from uf;
 
+create table municipio (
+	idmunicipio integer not null,
+	nome varchar(30) not null,
+	iduf integer not null,
+	
+	constraint pk_mnc_idmunicipio primary key (idmunicipio),
+	constraint un_mnc_nome unique (nome),
+	constraint fk_mnc_iduf foreign key (iduf) references uf (iduf)
+);
+
+insert into municipio (idmunicipio, nome, iduf) values (1, 'Porto União', 1);
+insert into municipio (idmunicipio, nome, iduf) values (2, 'Canoinhas', 1);
+insert into municipio (idmunicipio, nome, iduf) values (3, 'Porto Vitória', 2);
+insert into municipio (idmunicipio, nome, iduf) values (4, 'General Carneiro', 2);
+insert into municipio (idmunicipio, nome, iduf) values (5, 'São Paulo', 3);
+insert into municipio (idmunicipio, nome, iduf) values (6, 'Rio de Janeiro', 6);
+insert into municipio (idmunicipio, nome, iduf) values (7, 'Uberlândia', 4);
+insert into municipio (idmunicipio, nome, iduf) values (8, 'Porto Alegre', 5);
+insert into municipio (idmunicipio, nome, iduf) values (9, 'União da Vitória', 2);
+
 
 
 
