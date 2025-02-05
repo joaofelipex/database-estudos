840f073e3b4bbe9ae3c1f338e5178de9014d34b3
joaofelipex <felipemiranda981@gmail.com>
2025-02-05 18:58:35 -0300

    Commit de total avanço. Seguimos!!!

--- scriptestudos.sql
+++ scriptestudos.sql
@@ -298,7 +298,25 @@ insert into municipio (idmunicipio, nome, iduf) values (6, 'Rio de Janeiro', 6);
 insert into municipio (idmunicipio, nome, iduf) values (7, 'Uberlândia', 4);
 insert into municipio (idmunicipio, nome, iduf) values (8, 'Porto Alegre', 5);
 insert into municipio (idmunicipio, nome, iduf) values (9, 'União da Vitória', 2);
+select * from municipio;
 
+select * from cliente;
+alter table cliente drop municipio;
+alter table cliente drop uf;
+alter table cliente add idmunicipio integer;
+alter table cliente add constraint fl_cliente_idmunicipio foreign key (idmunicipio) references municipio (idmunicipio);
+
+update cliente set idmunicipio = 1 where idcliente in (1, 2, 10, 11);
+update cliente set idmunicipio = 2 where idcliente in (3, 12);
+update cliente set idmunicipio = 3 where idcliente = 4;
+update cliente set idmunicipio = 4 where idcliente in (5);
+update cliente set idmunicipio = 4 where idcliente in (6, 13);
+update cliente set idmunicipio = 4 where idcliente in (7);
+update cliente set idmunicipio = 4 where idcliente in (8);
+update cliente set idmunicipio = 4 where idcliente in (9);
+update cliente set idmunicipio = 4 where idcliente in (14, 15);
+
+select * from cliente;
 
 
 
