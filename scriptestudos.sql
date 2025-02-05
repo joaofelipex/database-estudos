5457cd95dd19230ec18d1b6d6fb9f7a0df781116
joaofelipex <felipemiranda981@gmail.com>
2025-02-05 15:57:01 -0300

    mais um save nesse arquivo .sql. que é para os estudos de database.

--- scriptestudos.sql
+++ scriptestudos.sql
@@ -221,6 +221,16 @@ alter table cliente drop idprofissao;
 alter table cliente add idprofissao integer; --foreign key
 alter table cliente add constraint fk_cln_idprofissao foreign key (idprofissao) references profissao (idprofissao);
 
+update cliente set idprofissao = 1 where idcliente in (1, 9, 10, 12, 15, 17);
+update cliente set idprofissao = 2 where idcliente = 2;
+update cliente set idprofissao = 3 where idcliente = 3;
+update cliente set idprofissao = 4 where idcliente in (4, 5);
+update cliente set idprofissao = 5 where idcliente in (6, 7, 8, 13);
+
+select * from profissao;
+delete from profissao where idprofissao = 10;
+insert into profissao (idprofissao, nome) values (10, 'Teste');
+
 
 
 
