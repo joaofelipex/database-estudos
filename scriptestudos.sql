dac8c9653c07caa390216451645927b248e756ea
joaofelipex <felipemiranda981@gmail.com>
2025-02-06 15:17:23 -0300

    atualização do scriptestudos, pois tinha erros de indicação.

--- scriptestudos.sql
+++ scriptestudos.sql
@@ -437,39 +437,37 @@ create table pedido (
 select * from pedido;
 
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (1, '2008-04-01', 1300, 'Manoel', 'BS. Transportes', 'André');
+values (1, '2008-04-01', 1300, 1, 1, 1);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (2, '2008-04-01', 500, 'Manoel', 'BS. Transportes', 'André');
+values (2, '2008-04-01', 500, 1, 1, 1);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (3, '2008-04-01', 300, 'Fernanda', 'União Transportes', 'Maria');
+values (3, '2008-04-02', 300, 11, 2, 5);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (4, '2008-04-01', 1000, 'Camila', 'BS. Transportes', 'Aline');
+values (4, '2008-04-05', 1000, 8, 1, 7);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (5, '2008-04-01', 200, 'Cristiano', 'BS. Transportes', 'Suelem');
+values (5, '2008-04-06', 200, 9, 2, 6);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (6, '2008-04-01', 1985, 'Fabrício', 'BS. Transportes', 'Suelem');
+values (6, '2008-04-06', 1985, 10, 1, 6);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (7, '2008-04-01', 800, 'Carlos', 'BS. Transportes', 'Aline');
+values (7, '2008-04-06', 800, 3, 1, 7);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (8, '2008-04-01', 175, 'Carlos', 'BS. Transportes', 'Aline');
+values (8, '2008-04-06', 175, 3, null, 7);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (9, '2008-04-01', 1300, 'Gilmar', 'BS. Transportes', 'Silvana');
+values (9, '2008-04-07', 1300, 12, null, 8);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (10, '2008-04-01', 200, 'Ângelo', 'BS. Transportes', 'Silvana');
+values (10, '2008-04-10', 200, 6, 1, 8);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (11, '2008-04-01', 300, 'Jéssica', 'BS. Transportes', 'André');
+values (11, '2008-04-15', 300, 15, 2, 1);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (12, '2008-04-01', 500, 'Jéssica', 'BS. Transportes', 'Maria');
+values (12, '2008-04-20', 500, 15, 2, 5);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (13, '2008-04-01', 350, 'Cristiano', 'BS. Transportes', 'Aline');
+values (13, '2008-04-20', 350, 9, 1, 7);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (14, '2008-04-01', 300, 'Geraldo', 'BS. Transportes', 'Maria');
+values (14, '2008-04-23', 300, 2, 1, 5);
 insert into pedido (idpedido, data_pedido, valor, idcliente, idtransportadora, idvendedor)
-values (15, '2008-04-01', 200, 'Fernanda', 'BS. Transportes', 'Maria');
+values (15, '2008-04-25', 200, 11, null, 5);
 
-
-
-
+select * from cliente;
 
 
 
