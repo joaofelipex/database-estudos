867e9e35604d35c8d4c7988b205c67b2937172d9
joaofelipex <felipemiranda981@gmail.com>
2025-02-06 16:57:40 -0300

    mais um update para o script de SQL, mais avanços. O que já foi feito: Criar, inserir, alterar e excluir dados de tabelas
    Implementar chaves primárias e chaves estrangeiras. Agora é avançar mais.

--- scriptestudos.sql
+++ scriptestudos.sql
@@ -469,6 +469,68 @@ values (15, '2008-04-25', 200, 11, null, 5);
 
 select * from cliente;
 
+create table pedido_produto (
+	idpedido integer not null,
+	idproduto integer not null,
+	quantidade integer not null,
+	valor_unitario numeric(10,2) not null,
+	
+	constraint pk_pdp_idpedidoproduto primary key (idpedido, idproduto),
+	constraint fk_pdp_idpedido foreign key (idpedido) references pedido (idpedido),
+	constraint fk_pdp_idproduto foreign key (idproduto) references produto (idproduto)
+);
+
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (1, 1, 1, 800);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (1, 2, 1, 500);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (2, 2, 1, 500);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (3, 4, 2, 150);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (4, 1, 1, 800);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (4, 3, 1, 200);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (5, 3, 1, 200);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (6, 1, 2, 800);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (6, 7, 1, 35);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (6, 5, 1, 200);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (6, 4, 1, 150);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (7, 1, 1, 800);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (8, 7, 5, 35);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (9, 1, 1, 800);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (9, 2, 1, 500);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (10, 5, 1, 200);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (11, 5, 1, 200);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (11, 6, 1, 100);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (12, 2, 1, 500);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (13, 3, 1, 200);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (13, 4, 1, 150);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (14, 6, 3, 100);
+insert into pedido_produto (idpedido, idproduto, quantidade, valor_unitario)
+values (15, 3, 1, 200);
+
+select * from pedido_produto;
+
+
+
 
 
 
